0ae4ed9c9743482f871f18106d50c036ba876444
unknown <kwdenman@gmail.com>
2019-04-13 13:40:42 -0800

    declaring variables

---
+++ app.js
@@ -0,0 +1,13 @@
+// var, let, const
+
+// Declaring a Variable
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Initializing a Variable
+var greeting;
+console.log(greeting);
+greeting = 'Hello World';
+console.log(greeting);
