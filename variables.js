8160ffe26330b12cc133afa3afcf15e67ee5acc7
unknown <kwdenman@gmail.com>
2019-04-18 22:49:10 -0800

    assigned variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+// var, let, const
+
+// Declaring a Variable
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Initializing a Variable
+var greeting;
+console.log(greeting);
+greeting = 'Hello World';
+console.log(greeting);
+
+const person = {
+    name: 'Kyle',
+    age: 37,
+    occupation: 'turd-sucker'
+};
+
+person.name = 'Sara';
+person.age = 32;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6); // .push adds a sixth number
+
+console.log(numbers);
